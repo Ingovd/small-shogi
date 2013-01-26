fe05f166799abaee65419ce3906a23834b268d8a
Ingovd <ingovanduijn@gmail.com>
2013-01-26 15:47:21 +0100

    what did I do?

--- code/csharp/small-shogi/small-shogi/search/PNSearch.cs
+++ code/csharp/small-shogi/small-shogi/search/PNSearch.cs
@@ -9,30 +9,32 @@ namespace smallshogi
 		static Func<Node, int> PN = n => n.pn;
 		static Func<Node, int> DN = n => n.dn;
 
+		public Hashtable transposition = new Hashtable();
+		public int nodeCount = 0;
+
+		public Node root;
+
 		public PNSearch ()
 		{
 		}
 
-		public int Search (Node root, Game g)
+		public int Prove (Game g)
 		{
+			root = new Node(g.startingPos, 1);
 			root.Evaluate(g);
 			int count = 0;
 			while (root.pn != 0 && root.dn != 0) {
 				var mpn = MostProving(root);
-				mpn.Expand (g);
-				Node.InitiateVisiting ();
-				mpn.Update (null);
-				//if(root.DetectDraw())
-				//	break;
+                Node.InitiateVisiting();
+                mpn.Expand(g, transposition);
+				Node.InitiateVisiting();
+				mpn.Update ();
 				if(count % 100 == 0) {
 					Node.InitiateVisiting ();
-					int size = root.Size();
 					Console.WriteLine(root.pn + " and " + root.dn);
-					Console.WriteLine("Transposition: " + Node.transposition.Count);
-					Console.WriteLine("Count:         " + size);
-					//Console.WriteLine("Draw:          " + (root.DetectDraw() ? "True" : "False"));
+					Console.WriteLine("Transposition: " + transposition.Count);
 				}
-				if(count >= 10000)
+				if(count >= 1000000)
 					break;
  				count++;
 			}
@@ -43,11 +45,17 @@ namespace smallshogi
 		public Node MostProving (Node v)
 		{
 			Node.InitiateVisiting ();
-			while (v.children != null && !v.IsVisited ())
+			while (v.children != null) {
 				if (v.c == 1)
 					v = LeftmostNode (v, PN);
 				else
 					v = LeftmostNode (v, DN);
+				if(v.IsVisited ()) {
+					v.pn *= 2;
+					v.dn *= 2;
+					break;
+				}
+			}
 			return v;
 		}
 
@@ -58,268 +66,6 @@ namespace smallshogi
 				if (f(child) == f(v) && !child.IsVisited ())
 					return child;
 			return v;
-		}
-	}
-
-	public class Node
-	{
-		public BitBoard[] position;
-		public List<Node> children;
-		public List<Node> parents = new List<Node> ();
-		public Node previous;
-		public byte c;
-		public int pn, dn, marker;
-		static int visitMarker;
-
-		public static Hashtable transposition = new Hashtable ();
-
-		public Node (BitBoard[] position, byte colour)
-		{
-			this.position = position;
-			this.c = colour;
-		}
-
-		public Node (Ply p, Node parent, byte colour)
-		{
-			this.position = p.apply (parent.position);
-			c = colour;
-		}
-
-		public static void InitiateVisiting ()
-		{
-			visitMarker++;
-		}
-
-		public void SetVisit ()
-		{
-			this.marker = Node.visitMarker;
-		}
-
-		public void UnVisit ()
-		{
-			this.marker--;
-		}
-
-		public bool IsVisited ()
-		{
-			return this.marker == Node.visitMarker;
-		}
-
-		public void Draw ()
-		{
-			pn = Int32.MaxValue;
-			dn = 0;
-			Node.InitiateVisiting ();
-			children = null;
-			foreach (var parent in parents)
-				parent.Update (null);
-		}
-
-		public bool DetectDraw ()
-		{
-			InitiateVisiting ();
-			return Drawn ();
-		}
-
-		public bool Drawn ()
-		{
-			// If this node has already been visited its a draw INCORRECT
-			if (IsVisited ())
-				return true;
-			SetVisit ();
-			// Check if this node is proven, return result
-			if (pn == 0 || dn == 0)
-				return pn > 0;
-			// Check if this is an unexpanded node, return no draw
-			if (children != null) {
-				// Return draw value of children
-				if (c == 1) {
-					foreach (var child in children)
-						if (!child.Drawn ())
-							return false;
-					return true;
-				}
-				foreach (var child in children)
-					if (child.Drawn ())
-						return true;
-			}
-			// Unvisit?
-			return false;
-		}
-
-		public void StartUpdate ()
-		{
-			UpdateNumbers ();
-
-			// Update parents
-			foreach (var parent in parents)
-				parent.Update (this);
-		}
-
-		public void Update (Node origin)
-		{
-			// Tree test, draw detected if true
-			/*if (origin != null && this.Equals (origin)) {
-				origin.Draw ();
-				return;
-			}*/
-
-			// DAG/DCG test, dont visit a node more than once
-			if(IsVisited ())
-				return;
-			SetVisit ();
-
-			UpdateNumbers ();
-
-			// Clear some memory maybe
-			//if(pn == 0 || dn == 0)
-			//	children = null;
-
-			// Update parents
-			foreach (var parent in parents)
-				parent.Update (origin);
-		}
-
-		public void UpdateNumbers ()
-		{
-			if (c == 1) {
-				int min = Int32.MaxValue;
-				int sum = 0;
-				foreach (var child in children) {
-					min = Math.Min (child.pn, min);
-					if(sum + child.dn >= sum)
-						sum += child.dn;
-					else
-						sum = Int32.MaxValue;
-				}
-				pn = min;
-				dn = sum;
-			} else {
-				int min = Int32.MaxValue;
-				int sum = 0;
-				foreach (var child in children) {
-					min = Math.Min (child.dn, min);
-					if(sum + child.pn >= sum)
-						sum += child.pn;
-					else
-						sum = Int32.MaxValue;
-				}
-				dn = min;
-				pn = sum;
-			}
-		}
-
-		public void Evaluate (Game g)
-		{
-			int pos = g.gamePosition (position, c);
-			if (pos > 0) {
-				pn = -1*(pos - 2) * Int32.MaxValue;
-				dn =    (pos - 1) * Int32.MaxValue;
-			} else {
-				pn = 1;
-				dn = 1;
-			}
-		}
-
-		public void Expand (Game g)
-		{
-			//if(children != null)
-			//	return;
-			children = new List<Node> ();
-			var plies = g.children (position, c);
-			foreach (var ply in plies) {
-				var s = new Node (ply, this, (byte)(c ^ 1));
-				/*s.Evaluate(g);
-				if(!transposition.ContainsKey(s))
-					transposition[s] = s;*/
-				if(transposition.ContainsKey(s))
-					s = (Node) transposition[s];
-				else {
-					s.Evaluate (g);
-					transposition[s] = s;
-				}
-				s.parents.Add(this);
-				children.Add (s);
-			}
-		}
-
-		public Node GetCycle ()
-		{
-			InitiateVisiting ();
-			return Cycle ();
-		}
-
-		public Node Cycle ()
-		{
-			if (IsVisited ()) {
-				foreach (var parent in parents)
-					parent.children.Remove(this);
-				return this;
-			}
-			SetVisit ();
-			if (children != null)
-				foreach (var child in children) {
-					var end = child.Cycle ();
-					if (end != null) {
-						child.previous = this;
-						return end;
-					}
-				}
-			UnVisit ();
-			return null;
-		}
-
-		public int Size ()
-		{
-			if(IsVisited ())
-				return 0;
-			SetVisit ();
-			var size = 1;
-			if(children != null)
-				foreach(var child in children)
-					size += child.Size ();
-			return size;
-		}
-
-		public void Show (Game g)
-		{
-			Console.WriteLine(g.prettyPrint(position));
-			if(children != null)
-				foreach(var child in children)
-					child.Show(g);
-		}
-
-		public override int GetHashCode ()
-		{
-			var hash = 982451653;
-			foreach (BitBoard b in position)
-				hash = 997 * hash + (int)b.bits;
-			hash ^= c * 2147483647;
-			return hash;
-		}
-
-		public override bool Equals (Object obj)
-		{
-			if (obj == null)
-				return false;
-			//if(System.Object.ReferenceEquals(this, obj))
-			//   return true;
-			Node node = obj as Node;
-			if (node == null)
-				return false;
-			return Equals (node);
-		}
-
-		public bool Equals (Node other)
-		{
-			if (c != other.c)
-				return false;
-			if (position.Length != other.position.Length)
-				return false;
-			for (int i = 0; i < position.Length; ++i)
-				if (!position [i].Equals (other.position [i]))
-					return false;
-			return true;
 		}
 	}
 }
